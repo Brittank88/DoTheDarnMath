27f83fb163b5fdc7250479ebe015d0aa226a10c0
Mason Hunt <masonhunt130ii@gmail.com>
2022-03-17 02:09:52 +1100

    Working build.gradle using ProGuard on the shadowed final jar.

---
+++ proguard-rules.pro
@@ -0,0 +1,3 @@
+-keep, includedescriptorclasses class com.brittank88.dtdm.** { *; }
+-dontobfuscate
+-optimizations library/*, class/unboxing/*, class/merging/*, field/removal/*, method/propagation/*, method/inlining/*, code/merging, code/simplification/*, code/removal/variable, code/removal/exception
--- src/main/java/com/brittank88/dtdm/util/command/UniversalSuggestionProvider.java
+++ src/main/java/com/brittank88/dtdm/util/command/UniversalSuggestionProvider.java
@@ -1,41 +1,57 @@
 package com.brittank88.dtdm.util.command;
 
+import com.mojang.brigadier.builder.RequiredArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.suggestion.Suggestion;
 import com.mojang.brigadier.suggestion.SuggestionProvider;
 import com.mojang.brigadier.suggestion.Suggestions;
 import com.mojang.brigadier.suggestion.SuggestionsBuilder;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Function;
 
 /**
- * Provides a simple way to generate a list of {@link com.mojang.brigadier.suggestion.Suggestion Suggestions}
- * for an {@link com.mojang.brigadier.builder.RequiredArgumentBuilder argument}, from a {@link Iterable<String>}<{@link String}>.
+ * Provides a simple way to generate a list of {@link Suggestion Suggestions}
+ * for an {@link RequiredArgumentBuilder argument}, from a {@link Iterable<String>}<{@link String}>.
  *
  * @see SuggestionProvider
  * @author Brittank88
- * @param <T> The type of the command source.
  */
-public record UniversalSuggestionProvider<T>(@NotNull Function<CommandContext<T>, Iterable<String>> suggestions) implements SuggestionProvider<T> {
+@SuppressWarnings("ClassCanBeRecord")
+public final class UniversalSuggestionProvider<T> implements SuggestionProvider<T> {
+
+    private final @NotNull Function<CommandContext<T>, Iterable<String>> suggestions;
 
     /**
      * Constructor for a {@link UniversalSuggestionProvider}.
      *
      * @param suggestions The {@link Iterable<String>}<{@link String}> to use as suggestions.
      */
-    public UniversalSuggestionProvider(@NotNull Function<@NotNull CommandContext<T>, @NotNull Iterable<@NotNull String>> suggestions) { this.suggestions = suggestions; }
+    public UniversalSuggestionProvider(@NotNull Function<@NotNull CommandContext<T>, @NotNull Iterable<@NotNull String>> suggestions) {
+        this.suggestions = suggestions;
+    }
 
     /**
-     * Builds and returns a {@link CompletableFuture<Suggestions>} containing all the {@link com.mojang.brigadier.suggestion.Suggestion Suggestions}.
+     * Builds and returns a {@link CompletableFuture<Suggestions>} containing all the {@link Suggestion Suggestions}.
      *
-     * @param ctx The {@link CommandContext} to apply to {@link com.mojang.brigadier.suggestion.Suggestion Suggestions}.
-     * @param builder The {@link SuggestionsBuilder} to add {@link com.mojang.brigadier.suggestion.Suggestion Suggestions} to.
-     * @return A built {@link CompletableFuture<Suggestions>} containing the {@link com.mojang.brigadier.suggestion.Suggestion Suggestions}.
+     * @param ctx The {@link CommandContext} to apply to {@link Suggestion Suggestions}.
+     * @param builder The {@link SuggestionsBuilder} to add {@link Suggestion Suggestions} to.
+     * @return A built {@link CompletableFuture<Suggestions>} containing the {@link Suggestion Suggestions}.
      */
-    @Override public @NotNull CompletableFuture<@NotNull Suggestions> getSuggestions(CommandContext<T> ctx, @NotNull SuggestionsBuilder builder) {
+    @Override
+    public @NotNull CompletableFuture<@NotNull Suggestions> getSuggestions(CommandContext<T> ctx, @NotNull SuggestionsBuilder builder) {
         this.suggestions.apply(ctx).forEach(builder::suggest);
         return builder.buildFuture();
     }
+
+    public @NotNull Function<CommandContext<T>, Iterable<String>> suggestions() { return suggestions; }
+
+    @Override
+    public int hashCode() { return Objects.hash(suggestions); }
+
+    @Override
+    public String toString() { return "UniversalSuggestionProvider[suggestions=" + suggestions + ']'; } //NON-NLS
 }
